024198b857f86bea0bd7520560d49124ef37c15b
rostom132 <tien.trandinh99@gmail.com>
2021-03-03 14:10:30 +0700

    measure temp with thread

--- device_app/device_app_main.py
+++ device_app/device_app_main.py
@@ -6,7 +6,7 @@ from collections import OrderedDict
 from threading import Thread
 from submodules.rgb_camera.rgb_camera import RgbCam
 from submodules.thermal_camera.thermal_camera import ThermalCam
-from submodules.face_detection.face_detection import FaceDetection
+from submodules.face_detection.face_detection import FaceDetection, LightFaceDetection, FaceDetectionLightRfb
 from submodules.object_tracking.objecttracking import CentroidTracker
 from submodules.object_tracking.Tracker import TrackableObject
 from submodules.measure_temperature.measure_temperature import measureTemperature
@@ -26,22 +26,27 @@ def faceRec(frame, faceTuple):
     embedding = ' '.join(map(str,encodings[0]))
     return embedding
 
+def create_measure_thread(objects, personID):
+    while personID in objects:
+        temperature = measureTemperature(color,lep.getFrame()[1], objects, personID, objects[personID].coor)
+        time.sleep(1)
+    #print('KILLED MEASURE')
+
 def create_send_thread(conn,objects, personID):
         while personID in objects:
-            print('send: ' + str(personID))
+            #print('send: ' + str(personID) + " " + str(objects[personID].coor))
             coor = objects[personID].coor
-            embedding = faceRec(frame, (coor[1], coor[2], coor[3], coor[0]))
+            embedding = faceRec(rgb.getOriginFrame(), (int(coor[1]*2.5), int(coor[2]*2.5), int(coor[3]*2.5), int(coor[0]*2.5)))
             conn.message_sending(personID, embedding, objects[personID])
-            time.sleep(3)
-        print('KILLED')
+            time.sleep(2)
+        #print('KILLED')
 
 def face_checking(frame,color,temp, objects,trackableObjects, rects, conn):
     for (objectID, obj) in objects.items():
         text = "ID {}".format(objectID)
         centroid = obj.coor
-        temperature =  measureTemperature(color,temp, objects, objectID, centroid)
+        
         y = centroid[1] - 10 if centroid[1] - 10 > 10 else centroid[1] + 10
-        cv2.putText(frame, str(temperature), (centroid[0], y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
         center = centroid_detect(centroid[0], centroid[1], centroid[2]-centroid[0], centroid[3]-centroid[1])
         if objectID in objects:
             cv2.putText(frame, text + obj.name, (center[0] - 10, center[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
@@ -52,9 +57,11 @@ def face_checking(frame,color,temp, objects,trackableObjects, rects, conn):
         # if there is no existing trackable object, create one
         if to is None:
             to = TrackableObject(objectID, centroid)
+            measure_thread = Thread(target=create_measure_thread, args=(objects, objectID,),daemon=True).start()
             send_thread = Thread(target=create_send_thread, args=(conn, objects, objectID,),daemon=True).start()
         elif (not to.counted):
             to.counted = True
+        cv2.putText(frame, str(obj.temperature), (centroid[0], y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
         trackableObjects[objectID] = to
 
 
@@ -63,16 +70,21 @@ def face_checking(frame,color,temp, objects,trackableObjects, rects, conn):
 CONNECTION_STRING = "HostName=thesis.azure-devices.net;DeviceId=device-test;SharedAccessKey=OYzTSkft0kli1VzJuoF9NIfw63tFqGKn3fFNuWroJbg="
 
 RGB_SOURCE = "nvarguscamerasrc ! video/x-raw(memory:NVMM), " \
-	"width=(int)960, height=(int)540,format=(string)NV12, " \
-	"framerate=(fraction)30/1 ! nvvidconv flip-method=2 ! video/x-raw, " \
+	"width=(int)1920, height=(int)1080,format=(string)NV12, " \
+	"framerate=(fraction)29/1 ! nvvidconv flip-method=2 ! video/x-raw, " \
 	"format=(string)BGRx ! videoconvert ! video/x-raw, " \
 	"format=(string)BGR ! appsink" 
 CAFFEMODEL = "./submodules/face_detection/models/res10_300x300_ssd_iter_140000.caffemodel"
 PROTOTEXTPATH = "./submodules/face_detection/models/deploy.prototxt.txt"
+MODEL = "./submodules/face_detection/models/slim-320.caffemodel"
+PROTO = "./submodules/face_detection/models/slim-320.prototxt"
+ONNX = "./submodules/face_detection/models/version-slim-320_simplified.onnx"
 
-rgb = RgbCam(RGB_SOURCE,640, 480)
+rgb = RgbCam(RGB_SOURCE,768, 432)
 lep = ThermalCam(640, 480)
-faceDetect = FaceDetection(model = CAFFEMODEL, proto = PROTOTEXTPATH)
+faceDetect = LightFaceDetection(PROTO, MODEL)
+#faceDetect = FaceDetection(CAFFEMODEL, PROTOTEXTPATH)
+#faceDetect = FaceDetectionLightRfb()
 ct = CentroidTracker()
 trackableObjects = {}
 objects = ct.update([])
@@ -83,15 +95,21 @@ while (1):
     raw = thermal
     thermal = cv2.resize(thermal,(640,480))
     color = cv2.applyColorMap(thermal, cv2.COLORMAP_JET)
+    #start = time.time()
     rects = faceDetect.detectFaces(frame)
+    #end = time.time()
     objects = ct.update(rects)
     face_checking(frame,color,temp, objects, trackableObjects, rects, conn)
     cv2.imshow('frame', frame)
     cv2.imshow('heat', color)
+    #print('Inference: {:.6f}s'.format(end-start))
     key = cv2.waitKey(1) & 0xFF
     # if the `q` key was pressed, break from the loop
     if key == ord("q"):
         rgb.capture.release()
+        #cv2.imwrite('thermal.png',thermal)
+        #cv2.imwrite('rgb.png', frame)
+        #cv2.imwrite('raw.png', raw)
         #lep.thread.join()
         cv2.destroyAllWindows()
         break
